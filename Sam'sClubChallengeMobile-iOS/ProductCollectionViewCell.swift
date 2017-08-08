8849b5ef744e64507d0aab91ba9f340dd7305645
Pin-Chih Lin <tobeaceo@gmail.com>
2017-08-07 17:28:39 -0700

    [Added] ProductCollectionViewCell.swift

---
+++ Sam'sClubChallengeMobile-iOS/ProductCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProductCollectionViewCell.swift
+//  Sam'sClubChallengeMobile-iOS
+//
+//  Created by Pin-Chih on 8/7/17.
+//  Copyright © 2017 Pin-Chih Lin. All rights reserved.
+//
+
+import UIKit
+
+class ProductCollectionViewCell:UICollectionViewCell{
+    
+    
+    
+    
+}
+
+
